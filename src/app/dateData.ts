010a2d5e5d0f230647a1e24346fcd4bbd7485b6e
madhur29shah87 <madhur29.shah@gmail.com>
2019-08-28 00:09:41 +0530

    Flight fare rendered + fetching logic + DD selection + disabled button

--- src/app/dateData.ts
+++ src/app/dateData.ts
@@ -2,6 +2,7 @@ export class DateData {
     todaysDay: Date;
     date: number;
     day: number;
+    month: number;
     yyyymmdd: string;
     flightName: string;
     flightPrice: number;
--- src/app/flight-calendar/flight-calendar.component.ts
+++ src/app/flight-calendar/flight-calendar.component.ts
@@ -14,16 +14,17 @@ export class FlightCalendarComponent implements OnInit {
   invalidRoute: boolean = false;
   rows: number[] = [0, 1, 2, 3, 4];
   columns: number[] = [0, 1, 2, 3, 4, 5, 6];
-
   days: String[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   dayItem: number = 0;
+
+  locationSelected: boolean = false;
 
   readonly ROOT_URL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/";
   readonly country = "US";
   readonly currency = "USD";
   readonly locale = "en-US";
-  // QUOTES_URL: string = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/" + this.country + "/" + this.currency + "/" + this.locale + "/";
-  ROUTE_URL: string = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/" + this.country + "/" + this.currency + "/" + this.locale + "/";
+  QUOTES_URL: string = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/" + this.country + "/" + this.currency + "/" + this.locale + "/";
+  //ROUTE_URL: string = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/" + this.country + "/" + this.currency + "/" + this.locale + "/";
 
   selectedOrigin: String = "SIN";
   selectedDestination: String = "KUL";
@@ -43,7 +44,7 @@ export class FlightCalendarComponent implements OnInit {
 
   getyyyymmdd(date) {
     let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-    let month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    let month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
     let year = date.getFullYear();
     return year + "-" + month + "-" + day;
   }
@@ -61,37 +62,42 @@ export class FlightCalendarComponent implements OnInit {
       "x-rapidapi-key": "cb1422ffc0msh6c2795240f66f6ep176724jsn5cf289092410",
       "content-type": "application/x-www-form-urlencoded"
     });
-    let tempUrl = this.ROUTE_URL + this.selectedOrigin + "/" + this.selectedDestination + "/" + this.outboundpartialdate;
+    let tempUrl = this.QUOTES_URL + this.selectedOrigin + "/" + this.selectedDestination + "/" + this.outboundpartialdate;
     // this.http.get(this.ROOT_URL + 'dade2fea-2399-4627-9127-46b2dcd2ea47',
     //   { headers: headers, params: { 'originAirports': 'SIN', 'destinationAirports': 'KUL' } }).subscribe();
 
-    this.http.get<any>(tempUrl, { headers: headers }).subscribe(data => this.sortData(data));
+    // this.http.get<any>(tempUrl, { headers: headers }).subscribe(data => this.sortData(data));
+    this.http.get<any>(tempUrl, { headers: headers }).subscribe((response) => {
+      this.sortData(response);
+    },
+      (error) => {
+        console.log('------->' + error.message);
+      });
   }
 
-  sortData(route) {
+  sortData(quotes) {
     let quoteArray: number[] = [];
-    // let carriers = routes.Carriers;
-    let quotes = route.Quotes;
-
-    // let smallestQuote = quoteArray[0];
-    // let legth = quoteArray.length;
-    // for (let i = 1; i < length; i++) {
-    //   if (quoteArray[i] < smallestQuote) {
-    //     smallestQuote = quoteArray[i];
-    //   }
-    // }
-
-    quotes.forEach(quote => {
-      // quoteArray.push(quote.MinPrice);
-      console.log('-->'+quote.MinPrice);
-      this.allDates.forEach(element => {
-        let dateData = element.filter(x => x.yyyymmdd === quote.OutboundLeg.DepartureDate.split('T')[0]);
-        if(dateData[0]){
-          dateData[0].flightPrice = quote.MinPrice;
-        }
+    // let quotes = route.Quotes;
+    if (quotes && quotes.Quotes && quotes.Quotes.length > 0) {
+      quotes.Quotes.forEach(quote => {
+        // quoteArray.push(quote.MinPrice);
+        console.log('-->' + quote.MinPrice);
+        this.allDates.forEach(element => {
+          let dateData = element.filter(x => x.yyyymmdd === quote.OutboundLeg.DepartureDate.split('T')[0]);
+          if (dateData[0]) {
+            dateData[0].flightPrice = quote.MinPrice;
+          }
+        });
       });
-    });
-
+    }
+    let nextDate = new Date(this.outboundpartialdate);
+    if (nextDate < (new Date(this.allDates[4][6].yyyymmdd))) {
+      nextDate.setDate(nextDate.getDate() + 1);
+      this.outboundpartialdate = this.getyyyymmdd(nextDate);
+      this.getFlightDetail();
+    } else {
+      this.outboundpartialdate = this.getTodaysDate();
+    }
   }
 
   constructor(private http: HttpClient) { }
@@ -117,6 +123,7 @@ export class FlightCalendarComponent implements OnInit {
       let yyyymmdd = this.getyyyymmdd(nextDay);
       this.dateData.date = nextDay.getDate();
       this.dateData.day = nextDay.getDay();
+      this.dateData.month = nextDay.getMonth();
       this.dateData.yyyymmdd = yyyymmdd;
       this.allDates[j] = (!this.allDates[j]) ? [] : this.allDates[j];
       this.allDates[j][k] = this.dateData;
@@ -127,31 +134,23 @@ export class FlightCalendarComponent implements OnInit {
       }
     }
   }
-  changeOrigin(obj) {
-    this.origin = obj;
+  changeOrigin(event) {
+    this.origin = event.target.innerText;
+    this.selectedOrigin = event.target.value;
     this.originDestValid();
   }
 
-  changeDestination(obj) {
-    this.destination = obj;
+  changeDestination(event) {
+    this.destination = event.target.innerText;
+    this.selectedDestination = event.target.value;
     this.originDestValid();
   }
 
   originDestValid() {
     if (this.origin != "Select Origin" && this.destination != "Select Destination") {
-      this.invalidRoute = (this.origin == this.destination) ? true : false;
+      this.invalidRoute = (this.origin === this.destination) ? true : false;
+      this.locationSelected = (this.origin !== this.destination);
     }
   }
-
-  // createArray(n: number): any[] {
-  //   return Array(n);
-  // }
-
-  // dayCounter(val) {
-  //   this.dateData.date = this.allDates[val].date;
-  //   this.dateData.day = this.allDates[val].day;
-  //   this.dayItem = val + 1;
-  //   return this.dateData.date;
-  // }
 
 }
